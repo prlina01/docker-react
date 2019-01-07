0b0644afc40ff3f4316ce40a641a557c7a76ceb5
Petar Prlina <petar.prlina@goluxtech.com>
2019-01-07 21:07:27 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            P E T A R
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://koncar.edu.rs"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Koncar
           </a>
         </header>
       </div>
